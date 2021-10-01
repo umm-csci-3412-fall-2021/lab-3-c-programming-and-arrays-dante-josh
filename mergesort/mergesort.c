f1188c1eab15a577b7b363f4522167ef72e32bc5
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-30 21:37:42 -0500

    Cleanup mergesort.c
    This commit makes mergesort.c neater by fixing the indentation and changing some variable names in order to make them clearer.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,53 +1,53 @@
 #include "mergesort.h"
 #include <cstdlib>
 
-void mergeRanges(int* value, int start_index, int midSize, int end_index){
-int rangeSize, firstIndex, secondIndex, copyIndex,i;
-rangeSize = end_index - start_index;
-int* destination = (int*)calloc(rangeSize, sizeof(int));
-firstIndex = start_index;
-secondIndex = midSize;
-copyIndex = 0;
-while(firstIndex < midSize && secondIndex < end_index){
-if(value[firstIndex] < value[secondIndex]){
-destination[copyIndex] = value[firstIndex];
-firstIndex++;
-}
-else{
-destination[copyIndex] = value[secondIndex];
-secondIndex++;
-}
-copyIndex++;
-}
-while(firstIndex<midSize){
-destination[copyIndex] = value[firstIndex];
-copyIndex++;
-firstIndex++;
-}
-while(secondIndex<end_index){
-destination[copyIndex] = value[secondIndex];
-copyIndex++;
-secondIndex++;
-}
-for(i=0; i<rangeSize;i++){
-value[i + start_index] = destination[i];
-}
-if(rangeSize != 0){
-        free(destination);
-    }
+void mergeRanges(int *values, int start_index, int midPoint, int end_index){
+	int rangeSize, firstIndex, secondIndex, copyIndex, i;
+	rangeSize = end_index - start_index;
+	int *destination = (int *)calloc(rangeSize, sizeof(int));
+	firstIndex = start_index;
+	secondIndex = midPoint;
+	copyIndex = 0;
+	while(firstIndex < midPoint && secondIndex < end_index) {
+		if(values[firstIndex] < values[secondIndex]){
+			destination[copyIndex] = values[firstIndex];
+			firstIndex++;
+		}
+		else{
+			destination[copyIndex] = values[secondIndex];
+			secondIndex++;
+		}
+		copyIndex++;
+	}
+	while(firstIndex < midPoint){
+		destination[copyIndex] = values[firstIndex];
+		copyIndex++;
+		firstIndex++;
+	}
+	while(secondIndex < end_index) {
+		destination[copyIndex] = values[secondIndex];
+		copyIndex++;
+		secondIndex++;
+	}
+	for(i=0; i < rangeSize; i++){
+		values[i + start_index] = destination[i];
+	}
+	if(rangeSize != 0) {
+		free(destination);
+	}
 }
 
-void mergesortRange(int* value, int start_index, int end_index){
- int rangeSize, midSize;
- rangeSize = end_index - start_index;
- if(rangeSize >= 2){
-         midSize = (start_index + end_index)/2;
-         mergesortRange(value,start_index,midSize);
-         mergesortRange(value,midSize,end_index);
-         mergeRanges(value,start_index,midSize,end_index);
-}}
+void mergesortRange(int *values, int start_index, int end_index) {
+	int rangeSize, midPoint;
+	rangeSize = end_index - start_index;
+	if(rangeSize >= 2) {
+		midPoint = (start_index + end_index)/2;
+		mergesortRange(values, start_index, midPoint);
+		mergesortRange(values, midPoint, end_index);
+		mergeRanges(values, start_index, midPoint, end_index);
+	}
+}
 
-
-void mergesort(int size, int* values) {
- mergesortRange(values,0,size);
+void mergesort(int size, int *values){
+	mergesortRange(values,0,size);
 }
