d131bec0fefeeb04d23609e00ec8299edc3346bf
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-10-04 21:35:02 -0500

    Work on implementation of array_merge
    This commit works on the implementation of array_merge. Right now it does not work, and we are trying to figure out why.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,46 @@
 #include "array_merge.h"
+#include "../mergesort/mergesort.h"
+#include <cstdlib>
+
+int get_array_size(int* array,int arrays_size){
+ int value = 0;
+ int i;
+ for(i = 0; i<arrays_size;i++){
+        value = value + array[i];
+        }
+ return value;
+}
+int find_elem(int elem, int* array, int size){
+ for( int i=0 ; i < size; i++){
+        if(array[i] == elem){
+                return true;
+        }
+    }
+ return false;
+}
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+  int i, old_value, new_value, array_size, j, counter, unique;
+  counter = 0;
+  unique = 0;
+  array_size = get_array_size(sizes,num_arrays);
+  int *destination = (int *)calloc(array_size+1, sizeof(int));
+  for(i = 0;i<num_arrays;i++){
+	  for(j = 0; j < sizes[i];j++){ 
+		if(unique == 0){
+		        destination[counter] = values[i][j];
+                        unique++;
+                        counter++;
+
+		}
+		else if(find_elem(values[i][j],destination,array_size+1) == false){ 
+	  		destination[counter] = values[i][j];
+			unique++;
+			counter++;
+		}
+	  }
+  }
+  mergesort(unique,destination);
+  destination[counter] = unique;
+  return destination;
 }
