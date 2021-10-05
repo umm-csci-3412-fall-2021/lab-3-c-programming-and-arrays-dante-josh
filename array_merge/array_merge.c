cb327eb6219513efaa91fec3ba7b7e00af6c54d6
Dante Miller <mill8622@rynite.morris.umn.edu>
2021-10-05 10:41:34 -0500

    Implement array_merge.c
    This commit finishes the implementation of array_merge.c. The tests now pass, but we still need to fix the memory leaks.
    
    Co-authored-by: Joshua Eklund <eklun124@umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,6 +2,7 @@
 #include "../mergesort/mergesort.h"
 #include <cstdlib>
 
+// function that calculates size of the arrays inside the array
 int get_array_size(int* array,int arrays_size){
  int value = 0;
  int i;
@@ -10,6 +11,9 @@ int get_array_size(int* array,int arrays_size){
         }
  return value;
 }
+
+
+// function that looks for the specified element and specified array and returns true if it is in there or false otherwise
 int find_elem(int elem, int* array, int size){
  for( int i=0 ; i < size; i++){
         if(array[i] == elem){
@@ -19,20 +23,28 @@ int find_elem(int elem, int* array, int size){
  return false;
 }
 
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  int i, old_value, new_value, array_size, j, counter, unique;
+  // creating variables to be used in the function
+  int i, array_size, j, counter, unique, zeroes;
+  // variables are set to zero
   counter = 0;
   unique = 0;
+  zeroes = 0;
+  // calls the get_array_size method on sizes and num_arrays
   array_size = get_array_size(sizes,num_arrays);
+  // allocate memory for destination
   int *destination = (int *)calloc(array_size+1, sizeof(int));
+  // for loop that iterates for the number of arrays given
   for(i = 0;i<num_arrays;i++){
+	  // for loop that iterates through the size of the arrays inside the array 
 	  for(j = 0; j < sizes[i];j++){ 
-		if(unique == 0){
-		        destination[counter] = values[i][j];
-                        unique++;
-                        counter++;
-
+		// check if current value is zero  
+		if(values[i][j] == 0) {
+			zeroes++;
 		}
+		// check if current value is already inside destination array
+		// if not, add it to destination, increment unique and counter
 		else if(find_elem(values[i][j],destination,array_size+1) == false){ 
 	  		destination[counter] = values[i][j];
 			unique++;
@@ -40,7 +52,32 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 		}
 	  }
   }
-  mergesort(unique,destination);
-  destination[counter] = unique;
-  return destination;
+
+  // check if zeroes is equal to zero
+  // if it is not, then increment unique
+  if(!(zeroes==0)){
+	  unique++;
+  }
+
+  // allocate memory for a new array that is the size of unique
+  int* secondArray = (int *)calloc(unique, sizeof(int));
+  // copy values from destination into firstArray
+  for(int i = 0; i < unique; i++){
+	  secondArray[i] = destination[i];
+  }
+
+  free(destination);
+  // sort the firstArray
+  mergesort(unique,secondArray);
+  // allocate memory for the final array that is the size of unique + 1
+  // we are doing unique + 1 to accomodate for the value of unique in front
+  int* theFinalArray = (int *)calloc(unique+1, sizeof(int));
+  // set the first value in theFinalArray to the value of unique
+  theFinalArray[0]=unique;
+  // copy values from firstArray into theFinalArray
+  for(int i = 1; i < unique+1; i++){
+          theFinalArray[i] = secondArray[i-1];
+  }
+  free(secondArray);
+  return theFinalArray;
 }
